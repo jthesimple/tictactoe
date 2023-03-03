01cc1848185eac4999f49a3f225a6998c3b07331
Julio <julio.bermudez619@gmail.com>
2023-03-02 18:08:02 -0800

    Updated winner function logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,43 +1,63 @@
 const playerFactory = (name, marker) =>{
     return {name, marker};
-
 };
 
 const gameboard = (() =>{
     const gameboardContents = [];
     let rowsContainer = [[0,0,0],[0,0,0]];
     let columnContainer = [[0,0,0],[0,0,0]];
-     
+    let diagonalContainer = [[0,0,0],[0,0,0]];
+    let oppositeDiagonalContainer = [[0,0,0],[0,0,0]];
 
     const checkWinner = function(row, column, marker){
         if (marker === 'X'){
             rowsContainer[0][row]+=1;
+            columnContainer[0][column]+=1;
+            if (row === column){
+                diagonalContainer[0][row] += 1;    
+            };
+            if (row + column + 1 ===3){
+                oppositeDiagonalContainer[0][row] +=1;
+            };
+            let totalSum = diagonalContainer[0].reduce((accumulator, currentValue) => accumulator + currentValue);
+            let oppositeTotalSum = oppositeDiagonalContainer[0].reduce((accumulator,currentValue) => accumulator + currentValue);
                 if (rowsContainer[0][row]=== 3){
                     console.log('Player X has won!');
-                }; 
-            columnContainer[0][column]+=1;
-                if (columnContainer[0][column]===3){
+                } else if (columnContainer[0][column]===3){
                     console.log('Player X has won!'); 
+                } else if (totalSum ===3){
+                    console.log('Player X has won!')
+                } else if (oppositeTotalSum ===3){
+                    console.log('Player X has won!')
                 };
         } else if (marker === 'O'){
             rowsContainer[1][row]+=1;
+            columnContainer[1][column]+=1;
+            if (row === column){
+                diagonalContainer[1][row] += 1;    
+            };
+            if (row + column + 1 ===3){
+                oppositeDiagonalContainer[1][row] +=1;
+            };
+            let totalSum = diagonalContainer[1].reduce((accumulator, currentValue) => accumulator + currentValue);
+            let oppositeTotalSum = oppositeDiagonalContainer[1].reduce((accumulator,currentValue) => accumulator + currentValue);
                 if (rowsContainer[1][row]=== 3){
                     console.log('Player O has won!');
-                };
-            columnContainer[1][column]+=1;
-                if (columnContainer[1][column]===3){
+                } else if (columnContainer[1][column]===3){
                     console.log('Player O has won!'); 
-                };      
-        }
-        
+                } else if (totalSum ===3){
+                    console.log('Player O has won!')
+                } else if (oppositeTotalSum ===3){
+                    console.log('Player O has won!')
+                };
+        };  
     };
-    return {checkWinner, rowsContainer, columnContainer};
+    return {checkWinner};
 })();
 
 
 const gameFlow = (() =>{
-
-    const buttons = document.querySelectorAll('button');
+    const buttons = document.querySelectorAll('button.game-button');
     let players = [playerFactory('julio', 'X'), playerFactory('alberto','O')];
 
     let activePlayer = players[0];
@@ -74,7 +94,6 @@ const gameFlow = (() =>{
 })();
 
 gameFlow.playGame();
-
 
 
 
